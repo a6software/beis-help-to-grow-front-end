9f8a35a322924e4808d892ca5867e0de231dbf9c
Christopher Moss <chris@a6software.co.uk>
2021-07-09 07:53:32 +0100

    Nunjucks button links

--- src/routes/create-account/index.ts
+++ src/routes/create-account/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
-import emailAddressRouter from './email-address';
-import passwordRouter from './set-password';
-import verificationRouter from './verification';
-import termsAndConditionsRouter from './terms-conditions';
 import accountCreatedRouter from './account-created';
 import authorisationRouter from './authorisation';
+import emailAddressRouter from './email-address';
+import passwordRouter from './set-password';
+import termsAndConditionsRouter from './terms-conditions';
+import verificationRouter from './verification';
+
 const router = express.Router();
 
-router.use('/email-address', emailAddressRouter);
-router.use('/set-password', passwordRouter);
-router.use('/verification', verificationRouter);
-router.use('/terms-conditions', termsAndConditionsRouter);
 router.use('/account-created', accountCreatedRouter);
 router.use('/authorisation', authorisationRouter);
+router.use('/email-address', emailAddressRouter);
+router.use('/set-password', passwordRouter);
+router.use('/terms-conditions', termsAndConditionsRouter);
+router.use('/verification', verificationRouter);
+
 export default router;
